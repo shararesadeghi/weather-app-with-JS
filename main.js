f4fb1458ec2fc3024f759f3b15f2f170d753eced
jamalshamsi <111067644+shararesadeghi@users.noreply.github.com>
2023-07-11 19:55:09 +0430

    api

---
+++ main.js
@@ -0,0 +1,2 @@
+// `https://s3-us-west-2.amazonaws.com/s.cdpn.io/162656/${weather[0]["icon"]}.svg`
+// api.openweathermap.org/data/2.5/weather?q=tehran&appid=edc228562ac0a8aa3116d41c0687cf56&units=metric
